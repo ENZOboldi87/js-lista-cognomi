3d2a208ba2c02d09f1b174a905cf3af322b32bf9
Giuseppe Rotolo <rotolo37@gmail.com>
2020-05-26 18:10:17 +0200

    esercizio completato, ma alcuni bug presenti

---
+++ js/myscript.js
@@ -0,0 +1,21 @@
+var listaCognomi = document.getElementById('lista-cognomi');
+
+
+// lista cognomi
+var cognomi = [ "Bianchi", "Rossi", "Duzioni", "Balsano", "Verdi"];
+
+
+do {
+  cognomeUtente = prompt ("inserisci il tuo cognome");
+  if (!isNaN(cognomeUtente)) {
+      alert("non hai inserito un cognome");
+    }
+
+
+} while ((0 > cognomeUtente.length) || (!isNaN(cognomeUtente)))  {
+  cognomi.push(cognomeUtente);
+  cognomi.sort();
+}
+
+alert('il tuo cognome e ' + cognomeUtente)
+alert('il tuo cognome e il numero ' + (cognomi.indexOf(cognomeUtente) + 1) + " su " + cognomi.length);
